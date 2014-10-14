bacae38a737ffa0ba9a2bb71053cf38aadac9ee5
Tamara Austin <tamara@openstile.com>
2014-10-13 22:39:09 -0400

    Move header and footer into application layout

--- spec/features/static_pages_spec.rb
+++ spec/features/static_pages_spec.rb
@@ -3,9 +3,19 @@ require 'rails_helper'
 describe "Static pages" do
   subject { page }
 
+  shared_examples "static_page" do
+    it { should have_link("Home", href: root_path) }
+    it { should have_link("About", href: about_path) }
+    #TODO add href for blog page
+    it { should have_link("Blog") }
+
+    it { should have_xpath("//footer") }
+  end
+
   describe "Home page" do
     before {visit '/'}
 
+    it_should_behave_like "static_page"
     it { should have_content('Explore Fashion Boutiques') }
     it { should have_title('A Personalized way to Explore Local Fashion Boutiques | OpenStile') }
   end
@@ -13,6 +23,7 @@ describe "Static pages" do
   describe "About page" do
     before {visit '/about'}
 
+    it_should_behave_like "static_page"
     it { should have_content('About us') }
     it { should have_title('About | OpenStile') }
   end
@@ -20,6 +31,7 @@ describe "Static pages" do
   describe "Prelaunch page" do
     before {visit '/prelaunch'}
 
+    it_should_behave_like "static_page"
     it { should have_content('Sign up for when we launch') }
     it { should have_title('Explore Washington DC Fashion Boutiques | OpenStile') }
   end
