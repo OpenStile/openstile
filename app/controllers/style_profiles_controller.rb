59c1c6841a428656c0d5e3490d122a6bc7a0c559
Tamara Austin <tamara@openstile.com>
2015-01-09 13:23:23 -0500

    Fixup style profiles edit route and test for proper authorization. Also removed the default authorization filters so that they should be explicitly set per controller

--- app/controllers/style_profiles_controller.rb
+++ app/controllers/style_profiles_controller.rb
@@ -1,4 +1,15 @@
 class StyleProfilesController < ApplicationController
+
+  before_filter :authenticate_shopper!
+  before_action :correct_style_profile_shopper
+
   def edit
   end
+
+  private
+    
+    def correct_style_profile_shopper
+      @style_profile = StyleProfile.find(params[:id])
+      redirect_to root_url unless (@style_profile.shopper == current_shopper)
+    end
 end
