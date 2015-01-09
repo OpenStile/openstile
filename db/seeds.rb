2bc9a70fca1073a2218e159a594320418afdaea3
Tamara Austin <tamara@openstile.com>
2015-01-09 10:17:34 -0500

    Create seeds for top, bottom, and dress sizes

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+["XS", "Small", "Medium", "Large", "XL"].each do |size|
+  TopSize.find_or_create_by(name: size, category: "alpha")
+  BottomSize.find_or_create_by(name: size, category: "alpha")
+  DressSize.find_or_create_by(name: size, category: "alpha")
+end
+
+["00", "0", "2", "4", "6", "8", "10", "12", "14"].each do |size|
+  TopSize.find_or_create_by(name: size, category: "numeric")
+  BottomSize.find_or_create_by(name: size, category: "numeric")
+  DressSize.find_or_create_by(name: size, category: "numeric")
+end
+
+["16", "18", "20"].each do |size|
+  BottomSize.find_or_create_by(name: size, category: "numeric")
+  DressSize.find_or_create_by(name: size, category: "numeric")
+end
+
+("24".."34").each do |size|
+  BottomSize.find_or_create_by(name: size, category: "inches")
+end
