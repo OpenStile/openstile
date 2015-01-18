da838354e3f9e972c449e88bb733b9007550cdae
Tamara Austin <tamara@openstile.com>
2015-01-17 21:04:24 -0500

    Style profile cleanup for size selection. Now numeric and inches options will only be shown on demand as alpha sizes are selected

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,13 +6,13 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-["X-Small", "Small", "Medium", "Large", "X-Large"].each do |size|
+["XS", "Small", "Medium", "Large", "XL"].each do |size|
   TopSize.find_or_create_by(name: size, category: "alpha")
   BottomSize.find_or_create_by(name: size, category: "alpha")
   DressSize.find_or_create_by(name: size, category: "alpha")
 end
 
-["00", "0", "2", "4", "6", "8", "10", "12", "14"].each do |size|
+["00", "0", "2", "4", "6", "8", "10", "12", "14", "16", "18"].each do |size|
   TopSize.find_or_create_by(name: size, category: "numeric")
   BottomSize.find_or_create_by(name: size, category: "numeric")
   DressSize.find_or_create_by(name: size, category: "numeric")
