c4da6f9f07eebbe5462a952880c341a5e738ffc7
elena <elena@stocktwits.com>
2015-02-05 19:31:33 -0800

    clean up whitespace in routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,7 @@ Rails.application.routes.draw do
 
   devise_for :shoppers, :skip => [:passwords, :registrations], path: '/shoppers', controllers: {
     sessions: 'shoppers/sessions'
-    } 
+    }
   devise_scope :shopper do
       get "/shoppers/sign_up", :to => "shoppers/registrations#new",   :as => 'new_shopper_registration'
       post "/shoppers", :to => "shoppers/registrations#create", :as => 'shopper_registration'
@@ -20,7 +20,7 @@ Rails.application.routes.draw do
 
   devise_for :retail_users, :skip => [:passwords, :registrations], path: '/retail_users', controllers: {
     sessions: 'retail_users/sessions'
-    } 
+    }
 
   resources :style_profiles, only: [:edit, :update]
 
