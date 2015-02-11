21138606b02eac5c24374ba9e16a241c0ba8ab21
Tamara Austin <tamara@openstile.com>
2015-02-11 00:23:53 -0500

    Allow admin user to create outfits for a retailer

--- app/controllers/outfits_controller.rb
+++ app/controllers/outfits_controller.rb
@@ -1,8 +1,32 @@
 class OutfitsController < ApplicationController
+  before_filter :authenticate_admin!, only: [:new, :create]
   
   def show
     @outfit = Outfit.find(params[:id])
     store_recommendation_show_url
   end
 
+  def new
+    @outfit = Retailer.find(params[:retailer_id]).outfits.build
+  end
+
+  def create
+    @outfit = Outfit.new(outfit_params)
+    if @outfit.save
+      flash[:success] = "Outfit added to catalog"
+      redirect_to catalog_retailer_path(@outfit.retailer) 
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def outfit_params
+    params.require(:outfit).permit(:name, :description, :price_description, :average_price,
+                            :retailer_id, :print_ids, :color_ids, :body_shape_id, :look_id,
+                            :for_petite, :for_tall, :for_full_figured, :top_fit_id,
+                            :bottom_fit_id, top_size_ids: [], bottom_size_ids: [],
+                            dress_size_ids: [], special_consideration_ids: [])
+  end
 end
--- spec/controllers/outfits_controller_spec.rb
+++ spec/controllers/outfits_controller_spec.rb
@@ -2,12 +2,31 @@ require 'rails_helper'
 
 RSpec.describe OutfitsController, :type => :controller do
   let(:outfit){ FactoryGirl.create(:outfit) }
+  let(:retail_user){ FactoryGirl.create(:retail_user, retailer: outfit.retailer) }
 
   context "when unauthenticated" do
     context "GET show" do
       it "returns http success" do
         get :show, {id: outfit.id}
         expect(response).to be_success
+      end
+    end
+  end
+
+  context "when non-admin user is signed in" do
+    before { sign_in retail_user }
+
+    context "GET new" do
+      it "redirects to signin" do
+        get :new
+        expect(response).to redirect_to(new_admin_session_path)
+      end
+    end
+
+    context "POST create" do
+      it "redirects to signin" do
+        post :create, {outfit: {name: 'Foo', description: 'Bar'}}
+        expect(response).to redirect_to(new_admin_session_path)
       end
     end
   end
