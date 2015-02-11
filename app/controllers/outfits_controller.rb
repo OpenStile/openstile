d50a33738a5113fac0aad104e18a2b07411f391b
Tamara Austin <tamara@openstile.com>
2015-02-11 00:54:31 -0500

    Fix strong parameter errors that were not setting prints, colors, top, and bottom fit

--- app/controllers/outfits_controller.rb
+++ app/controllers/outfits_controller.rb
@@ -24,9 +24,9 @@ class OutfitsController < ApplicationController
 
   def outfit_params
     params.require(:outfit).permit(:name, :description, :price_description, :average_price,
-                            :retailer_id, :print_ids, :color_ids, :body_shape_id, :look_id,
-                            :for_petite, :for_tall, :for_full_figured, :top_fit_id,
-                            :bottom_fit_id, top_size_ids: [], bottom_size_ids: [],
-                            dress_size_ids: [], special_consideration_ids: [])
+                            :retailer_id, :body_shape_id, :look_id, :top_fit_id, :bottom_fit_id,
+                            :for_petite, :for_tall, :for_full_figured, print_ids: [], color_ids: [], 
+                            top_size_ids: [], bottom_size_ids: [], dress_size_ids: [], 
+                            special_consideration_ids: [])
   end
 end
