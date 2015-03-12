51a9f9e9711bc99f31a495dca81a35593c817d79
Tamara Austin <tamara@openstile.com>
2015-03-11 20:05:29 -0400

    Changing host to openstile vs. heroku domain

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -55,7 +55,7 @@ Rails.application.configure do
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = "http://assets.example.com"
-  config.action_mailer.default_url_options = { :host => 'warm-brushlands-2230.heroku.com' }
+  config.action_mailer.default_url_options = { :host => 'openstile.com' }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
