bd1906ab2280457c22a9cc6bd7d90e29e0184cac
Tamara Austin <tamara@openstile.com>
2015-03-31 20:55:07 -0400

    Fix to keep retired items out of favorites tab

--- spec/features/style_feed_favorite_items_spec.rb
+++ spec/features/style_feed_favorite_items_spec.rb
@@ -2,11 +2,15 @@ require 'rails_helper'
 
 feature 'Style Feed' do
   let!(:outfit){ FactoryGirl.create(:outfit) }
+  let!(:old_favorite_outfit){ FactoryGirl.create(:outfit, status: 0) }
   let(:shopper){ FactoryGirl.create(:shopper) }
 
   scenario 'adding and removing favorites', js: true do
+    old_favorite_outfit.interested_shoppers << shopper
+
     given_i_am_a_logged_in_shopper shopper
     when_i_navigate_to_my_style_feed
+    then_my_style_feed_should_not_contain old_favorite_outfit, :favorites, false
     then_my_style_feed_should_not_contain outfit, :favorites, false
     when_i_toggle_my_like_for_an_item_in_my_style_feed outfit, "outfits_#{outfit.id}"
     then_my_style_feed_should_contain outfit, :favorites, false
