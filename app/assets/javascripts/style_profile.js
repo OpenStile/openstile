ceaa9d62adbb39c88f2272d17ac6de63b58c24e3
Tamara Austin <tamara.austin08@gmail.com>
2016-01-01 19:43:15 -0500

    Update style profile to be sleeker and use progress bar

--- app/assets/javascripts/style_profile.js
+++ app/assets/javascripts/style_profile.js
@@ -2,60 +2,89 @@
  * Created by tamaraaustin on 9/4/15.
  */
 
-function showFieldset(index){
-    var fieldsets = $('.fieldset-' + index);
-    if(fieldsets.length != 0){
-        $('fieldset').hide();
-        fieldsets.show();
-    }
-}
-
 $(document).ready(function(){
-    $('.style-form .fieldset .toggle').click(function(){
-        $(this).toggleClass('fa-caret-down')
-        $(this).toggleClass('fa-caret-right');
-        if ($(this).is('.fa-caret-down')) {
-            $(this).closest('.fieldset').find('.content').show();
-        } else {
-            $(this).closest('.fieldset').find('.content').hide();
-        }
+    //jQuery time
+    var current_fs, next_fs, previous_fs; //fieldsets
+    var left, opacity, scale; //fieldset properties which we will animate
+    var animating; //flag to prevent quick multi-click glitches
+
+    $(".style-profile .next").click(function(){
+        if(animating) return false;
+        animating = true;
+
+        current_fs = $(this).parent();
+        next_fs = $(this).parent().next();
+
+        //activate next step on progressbar using the index of next_fs
+        $(".style-profile .progressbar li").eq($(".fieldset-group").index(next_fs)).addClass("active");
+
+        //show the next fieldset
+        next_fs.show();
+        //hide the current fieldset with style
+        current_fs.animate({opacity: 0}, {
+            step: function(now, mx) {
+                //as the opacity of current_fs reduces to 0 - stored in "now"
+                //1. scale current_fs down to 80%
+                scale = 1 - (1 - now) * 0.2;
+                //2. bring next_fs from the right(50%)
+                left = (now * 50)+"%";
+                //3. increase opacity of next_fs to 1 as it moves in
+                opacity = 1 - now;
+                current_fs.css({'transform': 'scale('+scale+')'});
+                next_fs.css({'left': left, 'opacity': opacity});
+            },
+            duration: 800,
+            complete: function(){
+                current_fs.hide();
+                animating = false;
+            },
+            //this comes from the custom easing plugin
+            easing: 'easeInOutBack'
+        });
+        $('body').scrollTop(0);
     });
 
-    $('.style-form .img-selector').click(function() {
-        $(this).toggleClass('checked');
+    $(".style-profile .previous").click(function(){
+        if(animating) return false;
+        animating = true;
 
-        input_el = $('#'+$(this).data('id'));
+        current_fs = $(this).parent();
+        previous_fs = $(this).parent().prev();
+
+        //de-activate current step on progressbar
+        $(".style-profile .progressbar li").eq($(".fieldset-group").index(current_fs)).removeClass("active");
+
+        //show the previous fieldset
+        previous_fs.show();
+        //hide the current fieldset with style
+        current_fs.animate({opacity: 0}, {
+            step: function(now, mx) {
+                //as the opacity of current_fs reduces to 0 - stored in "now"
+                //1. scale previous_fs from 80% to 100%
+                scale = 0.8 + (1 - now) * 0.2;
+                //2. take current_fs to the right(50%) - from 0%
+                left = ((1-now) * 50)+"%";
+                //3. increase opacity of previous_fs to 1 as it moves in
+                opacity = 1 - now;
+                current_fs.css({'left': left});
+                previous_fs.css({'transform': 'scale('+scale+')', 'opacity': opacity});
+            },
+            duration: 800,
+            complete: function(){
+                current_fs.hide();
+                animating = false;
+            },
+            //this comes from the custom easing plugin
+            easing: 'easeInOutBack'
+        });
+        $('body').scrollTop(0);
+    });
+
+    $(".style-profile .img-selector").click(function(){
+        $(this).toggleClass('checked');
+        var input_el = $('#'+$(this).data('id'));
         input_el.prop('checked', !input_el.prop('checked'));
     });
-
-    $('.style-form fieldset').hide();
-    $('.style-form input#wizard-save').hide();
-    $('.style-form a#wizard-next').show();
-
-    var current_fieldset = 1;
-    var max_fieldsets = 5;
-
-    showFieldset(current_fieldset);
-
-    $('.style-form a#wizard-next').click(function(){
-        current_fieldset++;
-        $('.style-form a#wizard-back').show();
-        if(current_fieldset == max_fieldsets){
-            $('.style-form a#wizard-next').hide();
-            $('.style-form input#wizard-save').show();
-        }
-        showFieldset(current_fieldset);
-        $('body').scrollTop(0);
-    });
-
-    $('.style-form a#wizard-back').click(function(){
-        current_fieldset--;
-        $('.style-form a#wizard-next').show();
-        $('.style-form input#wizard-save').hide();
-        if(current_fieldset == 1){
-            $('.style-form a#wizard-back').hide();
-        }
-        showFieldset(current_fieldset);
-        $('body').scrollTop(0);
-    });
 });
+
+
