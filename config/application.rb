1301baba30d71e3209a93ac88a322f9c95321108
Tamara Austin <tamara.austin08@gmail.com>
2016-01-23 18:51:30 -0500

    Create mailer layout and convert devise mailers to use this layout

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,9 @@ module Openstile
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+
+    config.to_prepare do
+      Devise::Mailer.layout "mailer" # email.haml or email.erb
+    end
   end
 end
