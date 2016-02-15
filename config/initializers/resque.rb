aee4ec3617734d61b5e7dd5e70d4477b1d489182
Tamara Austin <tamara.austin08@gmail.com>
2016-02-14 21:53:22 -0500

    Fix typo for redis production cloud environment variable

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -1,6 +1,6 @@
 #config/initializers/resque.rb
 
-uri = URI.parse(ENV["REDISTOGO_URL"] || "redis://localhost:6379/" )
+uri = URI.parse(ENV["REDISCLOUD_URL"] || "redis://localhost:6379/" )
 
 Resque.redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
 Resque.after_fork = Proc.new { ActiveRecord::Base.establish_connection }
