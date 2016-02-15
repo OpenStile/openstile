15489852f4b8233b9aa840b87c6946d70cb3ba9a
Tamara Austin <tamara.austin08@gmail.com>
2016-02-14 21:46:45 -0500

    Support cloud based redis server for production

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -1,4 +1,6 @@
 #config/initializers/resque.rb
 
-Resque.redis = Redis.new
+uri = URI.parse(ENV["REDISTOGO_URL"] || "redis://localhost:6379/" )
+
+Resque.redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
 Resque.after_fork = Proc.new { ActiveRecord::Base.establish_connection }
